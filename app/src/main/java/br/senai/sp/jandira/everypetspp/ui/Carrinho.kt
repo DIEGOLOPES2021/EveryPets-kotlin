24186d48e024d15c01468e1769a4cfc32054c28b
Alex Costa <alex.vinicius4002@hotmail.com>
2021-11-08 08:43:43 -0300

    08/11/2021

---
+++ app/src/main/java/br/senai/sp/jandira/everypetspp/ui/Carrinho.kt
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.everypetspp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senai.sp.jandira.everypetspp.R
+
+class Carrinho : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carrinho)
+    }
+}
